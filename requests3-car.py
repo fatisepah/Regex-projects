8e6edbff979b20d01a7128e7a75c4166f614c16e
fatisepah <fatemeh.sepahvand2018@gmail.com>
2024-09-11 23:00:47 +0330

    changed

--- requests3-car.py
+++ requests3-car.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
+
 r = requests.get('https://www.sheypoor.com/s/iran/car/peugeot/207i-panorama-at?brandInSlug=43973', allow_redirects=False)
 print(r.text)
 
