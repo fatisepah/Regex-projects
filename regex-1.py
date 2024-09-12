db8f356bb87bd96f398e97bb7cb733ed03d330b7
fatisepah <fatemeh.sepahvand2018@gmail.com>
2024-09-12 13:56:04 +0330

    change passage in regex1

--- regex-1.py
+++ regex-1.py
@@ -5,12 +5,12 @@ print(re.findall(r'the price of oil is (\d+)\$ for (\d+)boshke',str))
 
 #################################
 
-str2='salam jadi. salam mehdi. salam sara. salam soosan.'
+str2='salam fafa. salam mehdi. salam sara. salam soosan.'
 result=re.search(r'salam',str2)
 
 #################################
 
-str3='salam jadi. salam mehdi. salam sara. salam soosan.'
+str3='salam fafa. salam mehdi. salam sara. salam soosan.'
 result2=re.sub(r'salam','Hi',str3)
 print(result2)
 print(str3)
